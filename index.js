0c9428a70ccec2a5d2cd32c5ef4279f725562961
0xkumi <dung.q@autonomous.nyc>
2019-03-16 07:01:26 +0700

    update

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const Async = require("async")
 const request = require("request")
 const url = process.argv["2"]
 
-let a= []
-for (let i=0; i<10000;i++){
+let a = []
+for (let i = 0; i < 10000; i++) {
 	a.push(i)
 }
 
-Async.mapLimit(a,40,(i,cb)=>{
-	request.get({url, followAllRedirects: false,followRedirect: false})
-	.on("error", function(err){
-		if (err) console.log(err)
-        
-	})
-	.on('response', function(response) {
+Async.mapLimit(a, 100, (i, cb) => {
+	var random = Math.floor(Math.random()*1000)
+	request.get({ url , headers: {"Range": `"${random}-${random}"`}})
+		.on("error", function (err) {
+			if (err) console.log(err)
+
+		})
+		.on('response', function (response) {
 			if (response.statusCode == 403) {
 				console.log(i)
 				process.exit(-1)
 			}
-		cb()
-	})
-},function(err,result){
+			cb()
+		})
+}, function (err, result) {
 })
